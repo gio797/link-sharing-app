42f71c6ac27c74c1db9c09e4321d7ed035ebbdc0
gio797 <g.muchaidze@yahoo.com>
2023-09-12 14:07:07 +0400

    api setup initial

---
+++ api/config/database.js
@@ -0,0 +1,15 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+const { Pool } = pkg;
+
+dotenv.config();
+
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+});
+
+export default pool;
